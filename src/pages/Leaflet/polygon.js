c1279250ce341aa6fe4013be40def8878b0ba34c
Nivedha Michalikova <nivedha.michalikova@quantec-group.com>
2022-08-25 09:29:40 +0200

    mapp obj added

--- src/pages/Leaflet/polygon.js
+++ src/pages/Leaflet/polygon.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useMemo, useRef } from "react";
+
+import { Map, TileLayer, FeatureGroup, Marker, Popup } from "react-leaflet";
+
+import useFullPageLoader from "hooks/useFullPageLoader";
 import Header from "components/Header";
 import L from "leaflet";
-import { Map, TileLayer, FeatureGroup } from "react-leaflet";
 
+import cities from "./cities.json";
 import { EditControl } from "react-leaflet-draw";
 import osm from "./osm-providers";
-import { useRef } from "react";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 delete L.Icon.Default.prototype._getIconUrl;
+const markerIcon = new L.Icon({
+  iconUrl: require("resources/images/marker.png"),
+  iconSize: [40, 40],
+  iconAnchor: [17, 46], //[left/right, top/bottom]
+  popupAnchor: [0, -46], //[left/right, top/bottom]
+});
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
@@ -18,12 +27,48 @@ L.Icon.Default.mergeOptions({
   shadowUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
 });
+
+
+
 const PolygonMap = () => {
-  const [center, setCenter] = useState({ lat: 53.621992, lng: 10.129395 });
+  const [center, setCenter] = useState({ lat: 13.084622, lng: 80.248357 });
   const [mapLayers, setMapLayers] = useState([]);
+  const [loader, showLoader, hideLoader] = useFullPageLoader();
+  const [totalItems, setTotalItems] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
 
   const ZOOM_LEVEL = 8;
   const mapRef = useRef();
+  const [windfarm, setWindfarm] = useState({});
+  const [windturbine, setWindturbine] = useState({});
+  const [lcu, setLcu] = useState({});
+  const [receiver, setReceiver] = useState({});
+  const [substation, setSubstation] = useState({});
+  const [radar, setRadar] = useState({});
+  const [comment, setComment] = useState({});
+  const getItemData = (type) => {
+    let itemData = sessionStorage.getItem(type);
+    if (itemData && itemData.length > 0) {
+      itemData = (itemData.length > 0) ? JSON.parse(itemData) : [];
+    }
+    return itemData;
+  };
+  useEffect(() => {
+    const getData = () => {
+      showLoader();
+      setWindfarm("windfarm");
+      setWindfarm("windturbine");
+      setWindfarm("lcu");
+      setWindfarm("receiver");
+      setWindfarm("substation");
+      setWindfarm("radar");
+      setWindfarm("comment");
+      hideLoader();
+    };
+    getData();
+  }, []);
+
 
   const _onCreate = (e) => {
     console.log(e);
@@ -96,6 +141,24 @@ const PolygonMap = () => {
               url={osm.maptiler.url}
               attribution={osm.maptiler.attribution}
             />
+       
+            {Object.keys(windturbine).map((item) => (
+              <Marker
+                position={[windturbine[item].latitude, windturbine[item].longitude]}
+                icon={markerIcon}
+                key={windturbine[item].id}
+              >
+                <Popup>
+                  <b>
+                    {windturbine[item].label}
+                    Latitude :  {windturbine[item].latitude}
+                    Latitude :  {windturbine[item].longitude}
+                  </b>
+                </Popup>
+              </Marker>
+            ))}
+
+
           </Map>
           {/* <pre className="text-left">{JSON.stringify(mapLayers, 0, 2)}</pre> -->
           */}
